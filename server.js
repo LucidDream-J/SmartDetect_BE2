acb34f97f50d3be4e54bf55eb1f4787e382fbb19
LucidDream <joseph_traff@hotmail.com>
2020-04-07 00:47:07 +0800

    fixed db connect url

--- server.js
+++ server.js
@@ -13,10 +13,8 @@ const image = require("./controllers/image");
 const db = knex({
   client: "pg",
   connection: {
-    host: "postgresql-shaped-74047",
-    user: "postgres",
-    password: process.env.PASSWORD,
-    database: "smart-detect"
+    connectionString : "process.env.DATABASE_URL",
+    ssl: true
   }
 });
 
